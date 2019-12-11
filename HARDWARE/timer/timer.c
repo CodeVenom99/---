7499255879d55c6bdd934b1b439b3e798c4eaa95
CodeVenom99 <1060401004@qq.com>
2019-12-11 13:18:58 +0800

    固定方向追踪

--- HARDWARE/timer/timer.c
+++ HARDWARE/timer/timer.c
@@ -10,10 +10,11 @@
 #include "24l01.h"
 int Encoder_Left,Encoder_Right;
 u16 send[8],value[8],array[8],i,j,k;;
-int Motor_A,Motor_B,target;
+int Motor_A,Motor_B,target_r,target_l;
 u8 send_date[32],t=0;
-float v_t,v_max=1.7;
+float v_t,v_max=1.7,a_t,a_max=0.1,v_targe_r,v_targe_l;
 extern int tiaojieA,Encoder_A_EXTI;	
+extern float angle[3];
 void TIM1_Int_Init(void)
 {
     TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
@@ -46,26 +47,33 @@ void TIM1_UP_IRQHandler(void)   //TIM1中断10ms
 {
 	if (TIM_GetITStatus(TIM1, TIM_IT_Update) != RESET)  //检查TIM3更新中断发生与否
 		{
-//	imu_date();		
+		imu_date();	
+		printf("z:%f\r\n",angle[2]);	
 		Distance();
 /********环形传感器检测***********/			
 //		t++;
 //		if(t==50)
 //		{
 //		LED=!LED;
-
 		date_change();
 		NRF24L01_TxPacket(send_date);//发送数据		
 //		t=0;				
 //		}
-		v_t=v_max*(array[1]-20.0)/(100.0-20.0);//设定线速度,40为允许最大距离，20为允许最小距离（可能为安全距离）
-		target=v_t*100.0/1.7;//理论上为130/1.7，线速度转换为编码器读数
+			
+		v_t=0.5;
+//		a_t=0;			
+//		v_t=v_max*(array[1]-20.0)/(100.0-20.0);//设定线速度,40为允许最大距离，20为允许最小距离（可能为安全距离）
+		a_t=a_max*(angle[2]-45.0)/(90.0-45.0);
+			
+		v_targe_l=v_t+a_t;v_targe_r=v_t-a_t;
+		target_l=v_targe_l*100.0/1.7;
+		target_r=v_targe_r*100.0/1.7;//理论上为130/1.7，线速度转换为编码器读数
+//		printf("l:%d r:%d \r\n",target_l,target_r);
 		Encoder_Left=Read_Encoder(2);	
-		Encoder_Right=Read_Encoder(3);	
-		Motor_A=Incremental_PI_A(Encoder_Left,target);		
-		Motor_B=Incremental_PI_B(Encoder_Right,target);	
-		
-//		printf("%d\r\n",Encoder_Left,Motor_B);
+		Encoder_Right=Read_Encoder(3);
+//		printf("Encoder_Left:%d Encoder_Right:%d \r\n",Encoder_Left,Encoder_Right);		
+		Motor_A=Incremental_PI_A(Encoder_Left,target_l);		
+		Motor_B=Incremental_PI_B(Encoder_Right,target_r);	
 		Xianfu_Pwm();	
 		Set_Pwm_Motor1(Motor_A);//左	
 		Set_Pwm_Motor2(Motor_B);
@@ -78,4 +86,4 @@ void TIM1_UP_IRQHandler(void)   //TIM1中断10ms
 
 //		TIM3 -> CNT=0;Encoder_A_EXTI=0;
 
-//		Motor_A=Motor_B=PID_position(distance,20);
+
--- USER/main.c
+++ USER/main.c
@@ -18,7 +18,7 @@
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); 
 
 	uart_init(115200);//优先级3
-//	uart3_init(115200);//imu ,优先级2
+	uart3_init(115200);//imu ,优先级2
 		LED_Init();
 		SPI2_Init();
 		NRF24L01_Init();
@@ -26,7 +26,7 @@
 	 
 	Encoder_Init();//优先级1
 	Motor_Init();//电机满幅7200
-// 
+ 
 	ADC1_GPIO_Config();
 	ADC1_Mode_Config();
 	NRF24L01_TX_Mode();	 
